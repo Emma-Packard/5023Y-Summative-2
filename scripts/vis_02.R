e1ddc76f877f5c3a1eb34574f66eb5363517943d
emma-packard <kby21jgu@uea.ac.uk>
2023-05-01 16:48:48 +0000

    making and checking models

--- scripts/vis_02.R
+++ scripts/vis_02.R
@@ -35,20 +35,9 @@ ggplot(filter_cricket,
   geom_smooth(method = "lm", se = FALSE, fullrange = TRUE)
 # diet does not affect starting mass 
 
-# how diet affects starting mass
+#________________________________----
 
-ggplot(filter_cricket, 
-       aes(x= diet, 
-           y= starting_mass,
-           fill=pronotum,
-           colour=pronotum)) +
-  geom_point()+
-  geom_smooth(method = "lm", se = FALSE, fullrange = TRUE)
-# diet does not affect starting mass starting mass:pronotum size
-
-#_______________________________________----
-
-#change in wieght and diet ----
+#change in weight and diet ----
 
 # change/ song / diet 
 filter_cricket %>% 
@@ -119,27 +108,65 @@ lsmodel1 <- lm(change_in_weight~diet + starting_mass + song_week + pronotum + di
 
 
 lsmodel1 %>% # summary, including conf,int 
-  broom::tidy(conf.int = T)
-
-#checking the p values
-broom::tidy(lsmodel1)
-
-
+  broom::tidy(conf.int = T) # 95% conf. int
+#song_week, pronotum, diet:starting mass, song_week:pronotum, starting_mass:pronotum and starting_mass:song - are not signiifcant 
 
 # performance check
 pdf("check_lsmodel1.pdf")
 performance::check_model(lsmodel1)
-dev.off() 
+dev.off() # seeing the fit of the model 
 
-#drop1 seeing what needs to be dropped
-
+#AIC 
 drop1(lsmodel1, test = "F")
+step(lsmodel1, direction = "both", method="AIC", trace = TRUE)
+# removing diet:song_week, due to the highest AIC 
+
+lsmodel2 <- lm(change_in_weight~diet + starting_mass + song_week + pronotum + diet:starting_mass + diet:pronotum + song_week:pronotum + starting_mass:pronotum + starting_mass:song_week, data = filter_cricket)
+
+pdf("check_lsmodel2.pdf")
+performance::check_model(lsmodel1)
+dev.off() # seeing the fit of the model 
 
 
+# removing the 15 instead 
+lsmodel1 <- lm(change_in_weight~diet + starting_mass + song_week + pronotum +diet:song_week + diet:pronotum, data = filter_cricket)
+
+pdf("check_lsmodel3.pdf")
+performance::check_model(lsmodel1)
+dev.off() # seeing the fit of the model 
+
+
+drop1(lsmodel3, test = "F")
+step(lsmodel3, direction = "both", method="AIC", trace = TRUE)
+
+lsmodel3 %>% # summary, including conf,int 
+  broom::tidy(conf.int = T) 
+
+#dropping starting_mass as it has the lowest AIC
+lsmodel4 <- lm(change_in_weight~diet+ song_week + pronotum +diet:song_week + diet:pronotum, data = filter_cricket)
+
+lsmodel4 %>% # summary, including conf,int 
+  broom::tidy(conf.int = T)
+
+drop1(lsmodel4, test = "F")
+step(lsmodel4, direction = "both", method="AIC", trace = TRUE)
+
+pdf("check_lsmodel4.pdf")
+performance::check_model(lsmodel4)
+dev.off() # seeing the fit of the model 
+
+# dropping diet:pronotum
+
+lsmodel5 <- lm(change_in_weight~diet+ song_week + pronotum +diet:song_week,  data = filter_cricket)
+
+pdf("check_lsmodel5.pdf")
+performance::check_model(lsmodel5)
+dev.off() # seeing the fit of the model 
+
+drop1(lsmodel5, test = "F")
+step(lsmodel5, direction = "both", method="AIC", trace = TRUE)
+
+lsmodel6 <- lm(change_in_weight~diet+ song_week + pronotum +diet:song_week,  data = filter_cricket)
 
 #_______________________________
 
-lsmodel2 <- lm(change_in_weight~diet + starting_mass + song_week + pronotum, data = filter_cricket)
-
-#performance check 
- performance::check_posterior_predictions(lsmodel2)
