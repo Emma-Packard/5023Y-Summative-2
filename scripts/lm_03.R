ffd13a016a6cdf16fc70e090d8c3070cfa3d685d
emma-packard <kby21jgu@uea.ac.uk>
2023-05-02 14:26:02 +0000

    more models

--- scripts/lm_03.R
+++ scripts/lm_03.R
@@ -14,7 +14,9 @@ lsmodel6 %>%
 performance::check_model(lsmodel6)# seeing the fit of the model
 
 drop1(lsmodel6, test = "F")# can look at the AIC 
-
+# these looked different form the others 
+# diet:song_week f= 5.3102, df=526,1, p=0.02159
+# diet:pronotum f = 4.9192, df=526,1, p=0.02699
 summary(lsmodel6)
 
 
@@ -30,4 +32,32 @@ performance::check_model(lsmodel7)# seeing the fit of the model
 drop1(lsmodel7, test = "F")# can look at the AIC 
 
 summary(lsmodel7)
+#diet:pronotum f=14.3365
 
+
+
+#dropping diet:pronotum from the model
+
+lsmodel8<- lm(change_in_weight~diet + starting_mass + song_week + pronotum + song_week:pronotum + starting_mass:pronotum + starting_mass:song_week,data = filter_cricket)
+
+lsmodel8 %>% 
+  broom::tidy(conf.int = F)#add 95% conf intervals
+
+performance::check_model(lsmodel8)# seeing the fit of the model
+
+drop1(lsmodel8, test = "F")# can look at the AIC 
+
+summary(lsmodel8)
+
+# dropped the wrong ones - starting again -
+
+lsmodel9<- lm(change_in_weight~diet + starting_mass + song_week + pronotum + diet:starting_mass + diet:song_week + diet:pronotum + song_week:pronotum + starting_mass:pronotum + starting_mass:song_week,data = filter_cricket)
+
+lsmodel9 %>% 
+  broom::tidy(conf.int = F)#add 95% conf intervals
+
+performance::check_model(lsmodel9)# seeing the fit of the model
+
+drop1(lsmodel9, test = "F")# can look at the AIC 
+
+summary(lsmodel9)
