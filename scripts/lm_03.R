5e05aa3f03db8a59ff38d653a1b47ce26cf395ad
emma-packard <kby21jgu@uea.ac.uk>
2023-05-02 14:52:01 +0000

    final model

--- scripts/lm_03.R
+++ scripts/lm_03.R
@@ -61,3 +61,48 @@ performance::check_model(lsmodel9)# seeing the fit of the model
 drop1(lsmodel9, test = "F")# can look at the AIC 
 
 summary(lsmodel9)
+
+#removing diet and starting_mass - due to a high p value
+
+lsmodel10<- lm(change_in_weight~diet + starting_mass + song_week + pronotum + diet:song_week + diet:pronotum + song_week:pronotum + starting_mass:pronotum + starting_mass:song_week,data = filter_cricket)
+
+lsmodel10 %>% 
+  broom::tidy(conf.int = F)#add 95% conf intervals
+
+performance::check_model(lsmodel10)# seeing the fit of the model
+
+drop1(lsmodel10, test = "F")# can look at the AIC 
+
+summary(lsmodel10)
+
+
+#droped starting_mass:song_week
+
+lsmodel11<- lm(change_in_weight~diet + starting_mass + song_week + pronotum + diet:song_week + diet:pronotum + song_week:pronotum + starting_mass:pronotum,data = filter_cricket)
+
+
+
+lsmodel11 %>% 
+  broom::tidy(conf.int = F)#add 95% conf intervals
+
+performance::check_model(lsmodel11)# seeing the fit of the model
+
+drop1(lsmodel11, test = "F")# can look at the AIC 
+
+summary(lsmodel11)
+
+
+# droped starting_mass:pronotum
+
+lsmodel12<- lm(change_in_weight~diet + starting_mass + song_week + pronotum + diet:song_week + diet:pronotum + song_week:pronotum, data = filter_cricket)
+
+
+lsmodel12 %>% 
+  broom::tidy(conf.int = F)#add 95% conf intervals
+
+performance::check_model(lsmodel12)# seeing the fit of the model
+
+drop1(lsmodel12, test = "F")# can look at the AIC 
+
+summary(lsmodel12)
+
