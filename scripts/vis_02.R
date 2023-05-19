cdb3bc287026a9dfd04cc5afffb7216ce0d89293
emma-packard <kby21jgu@uea.ac.uk>
2023-05-19 20:50:00 +0000

    moving stuff around

--- scripts/vis_02.R
+++ scripts/vis_02.R
@@ -28,6 +28,7 @@ geom_smooth(method = "lm", se = FALSE, fullrange = TRUE) # adding a line of best
 # as diet % increases so does the change in weight, and the amount of singing increases
 
 # start/change/diet
+scatter_starting <- 
 filter_cricket %>% 
   ggplot(aes(x=starting_mass,
              y=change_in_weight,
@@ -59,4 +60,106 @@ filter_cricket %>%
   geom_smooth(method = "lm", se = FALSE, fullrange = TRUE)
 
 # the lower the diet percentage, and the higher the starting mass the bigger the change in weight is 
+# 
 #____________________________----
+
+#_____________________________
+#dentsity plot test one
+
+weight_starting_scatterplot <- 
+  ggplot(filter_cricket, 
+         aes(x= starting_mass, 
+             y= change_in_weight)) +
+  geom_point()
+
+starting_mass_marginal<-# density plot that will be at the side of the graph
+  filter_cricket%>%  
+  ggplot()+
+  geom_density(aes(x=starting_mass), fill="purple")+
+  theme_void()
+
+
+change_in_weight_marginal <- # density plot that is at the side of the plot
+  filter_cricket %>% 
+  ggplot()+
+  geom_density(aes(x=change_in_weight), fill="green")+
+  theme_void()+
+  coord_flip()
+
+layout <- "
+AA#
+BBC
+BBC"
+
+
+starting_mass_marginal+weight_starting_scatterplot+change_in_weight_marginal+ # order of plots is important
+  plot_layout(design=layout)
+
+#_________________
+
+#no_0 <- filter_cricket %>% 
+#  filter(song_week>0)# removed 0s for this plot as a test
+
+weight_starting_scatterplot2 <- 
+  ggplot(filter_cricket, 
+         aes(x= song_week, 
+             y= change_in_weight, 
+             colour= factor(diet))) +
+  theme_classic()+
+  theme(legend.position = "none")+# removes the fig legend
+  geom_point()+
+  geom_smooth(method = "lm", se = FALSE, fullrange = TRUE)
+
+
+change_marginal <- 
+  filter_cricket %>% 
+  ggplot(aes(
+    x=change_in_weight,
+    colour = factor(diet), 
+    fill= factor(diet), 
+    alpha = 0.1,
+    bandwidth = 175))+
+  geom_density()+
+  theme_void()+
+  coord_flip()+
+  scale_alpha(guide = 'none') # removes the alpha legend  
+
+
+song_marginal <- 
+  filter_cricket %>% 
+  ggplot(aes(
+    x=song_week,
+    colour = factor(diet), 
+    fill= factor(diet), 
+    alpha = 0.1,
+    bandwidth = 175))+
+  geom_density()+
+  theme_void()+
+  theme(legend.position = "none")# removes the fig legend
+
+
+
+layout <- "
+AA#
+BBC
+BBC"
+
+
+song_marginal+weight_starting_scatterplot2+change_marginal+ # order of plots is important
+  plot_layout(design=layout)
+
+# change labs, including factor(diet)
+# plot themes /
+
+#____________________________----
+
+
+ggplot(filter_cricket, 
+       aes(x= song_week, 
+           y= change_in_weight, 
+           colour= factor(diet))) +
+  theme_classic()+
+  theme(legend.position = "none")+# removes the fig legend
+  geom_point()+
+  facet_wrap(~ diet)+
+  geom_smooth(method = "lm", se = FALSE, fullrange = TRUE)
